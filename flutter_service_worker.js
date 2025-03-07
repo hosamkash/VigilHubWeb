cf0afdb1c3923fa10fc9c130f5e10e76a4111df8
Administrator <eng.hosamkash@gmail.com>
2025-03-07 23:36:18 +0200

    new version 7-3-2025

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -116,7 +116,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "1765d39606b36a47aaf6b6915d7f2449
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "ee89d54f0b778b4807beb51acf85cb15",
+"flutter_bootstrap.js": "3d1aa8e2b1100674be161fa4b974835b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
