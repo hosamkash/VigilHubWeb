dc507e58f40954fe721c852edb74859f9b91bfb9
Administrator <eng.hosamkash@gmail.com>
2025-04-05 23:09:44 +0200

    upload 05-04-2025

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4187843609"
+    serviceWorkerVersion: "3318670084"
   }
 });
